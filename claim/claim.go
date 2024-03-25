cddf19c30d661266fd9cf43f4c37c228bd057805
123jiaru <1179530478@qq.com>
2024-03-26 03:04:52 +0800

    优化了领取接口中 redis watch 中重试机制的逻辑，避免极端情况的无限次数、无限时间的重试

--- claim/claim.go
+++ claim/claim.go
@@ -137,11 +137,20 @@ func main() {
 
 }
 
+// ClaimPrize 用于领取奖品的函数，传入一个 Redis 客户端 RD，返回可能的错误
 func ClaimPrize(RD *redis.Client) error {
-	var prizes int
+	var prizes int                 // 声明奖品数量变量
+	maxRetries := 3                // 最大重试次数
+	maxDuration := 5 * time.Second // 最大执行时间限制，假设为5秒
+
+	retries := 0        // 初始化重试次数为0
+	start := time.Now() // 记录开始时间
+
 	for {
 		err := RD.Watch(func(tx *redis.Tx) error {
 			var err error
+
+			// 从 Redis 中获取奖品数量
 			prizes, err = tx.Get("prizes").Int()
 			if err != nil {
 				return err
@@ -150,7 +159,7 @@ func ClaimPrize(RD *redis.Client) error {
 			// 如果奖品数量大于 0，则递减奖品数量
 			if prizes > 0 {
 				_, err = tx.Pipelined(func(pipe redis.Pipeliner) error {
-					// 在 redis 减少奖品数量
+					// 在 Redis 中递减奖品数量
 					pipe.Decr("prizes")
 					return nil
 				})
@@ -159,14 +168,21 @@ func ClaimPrize(RD *redis.Client) error {
 				}
 				return nil
 			}
-			return errors.New("奖品已经领完了")
+			return errors.New("奖品已经领完了") // 如果奖品数量为0，则返回错误信息
 		}, "prizes")
 
 		if err == nil {
 			// 如果没有错误，表示奖品数量获取和递减成功，跳出循环
 			break
 		} else if err == redis.TxFailedErr {
+			fmt.Print("当前有其他事务对 prizes 键进行了修改，事务回滚，并进行重试")
+
 			// 如果出现 redis.TxFailedErr 错误，表示事务失败，需要重试
+			retries++
+			if retries >= maxRetries || time.Since(start) >= maxDuration {
+				// 如果达到最大重试次数或者超过最大时间限制，退出循环并返回错误信息
+				return errors.New("重试次数超过限制或执行时间超时")
+			}
 			continue // Retry
 		} else {
 			// 如果出现其他错误，返回给客户端错误信息，并结束处理
