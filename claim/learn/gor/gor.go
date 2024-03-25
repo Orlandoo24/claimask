431d9927876c897a0be19382d2b7eddc52872725
123jiaru <1179530478@qq.com>
2024-03-26 03:04:03 +0800

    优化了测试脚本，时间计算更为准确

--- claim/learn/gor/gor.go
+++ claim/learn/gor/gor.go
@@ -13,20 +13,22 @@ import (
 // 请求的目标地址
 const targetURL = "http://127.0.0.1:8870/claim"
 
-// 声明一个互斥锁，用于保护请求计数器和总时间
+// 声明一个互斥锁，用于保护总时间和请求计数
 var (
-	counterMutex sync.Mutex
-	requestCount int
-	totalTime    time.Duration
+	totalTime time.Duration
+	mutex     sync.Mutex
 )
 
 func main() {
 	// 设置随机数种子
 	rand.Seed(time.Now().UnixNano())
 
+	// 记录开始时间
+	startTime := time.Now()
+
 	// 并发发起10,000个请求
 	var wg sync.WaitGroup
-	for i := 0; i < 20000; i++ {
+	for i := 0; i < 100; i++ {
 		wg.Add(1)
 		go func() {
 			sendRequest()
@@ -35,14 +37,15 @@ func main() {
 	}
 	wg.Wait()
 
+	// 计算总时间
+	totalTime = time.Since(startTime)
+
 	// 输出总时间
 	fmt.Printf("所有请求完成\n总时间：%.2f秒\n", totalTime.Seconds())
 }
 
 // 发送HTTP请求
 func sendRequest() {
-	startTime := time.Now()
-
 	// 生成随机地址
 	randomAddress := generateRandomAddress()
 
@@ -61,14 +64,6 @@ func sendRequest() {
 		return
 	}
 	defer resp.Body.Close()
-
-	// 记录请求完成时间并增加请求计数器
-	endTime := time.Now()
-	increaseRequestCount()
-	addTotalTime(endTime.Sub(startTime))
-
-	// 打印请求结果
-	fmt.Printf("请求地址：%s，状态码：%d\n", randomAddress, resp.StatusCode)
 }
 
 // 生成随机地址
@@ -76,20 +71,4 @@ func generateRandomAddress() string {
 	// 生成随机数作为地址
 	randomNumber := rand.Intn(1000)
 	return fmt.Sprintf("Address%d", randomNumber)
-}
-
-// 增加请求计数器
-func increaseRequestCount() {
-	// 使用互斥锁保护请求计数器
-	counterMutex.Lock()
-	defer counterMutex.Unlock()
-	requestCount++
-}
-
-// 增加总时间
-func addTotalTime(duration time.Duration) {
-	// 使用互斥锁保护总时间
-	counterMutex.Lock()
-	defer counterMutex.Unlock()
-	totalTime += duration
 }
